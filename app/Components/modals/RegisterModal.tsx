30ebe8bcd741cd5b4bb79ebe59324b04af33b39f
bjorn0001 <ken@farmart.co>
2023-08-28 13:03:50 +0530

    login form ui

--- app/Components/modals/RegisterModal.tsx
+++ app/Components/modals/RegisterModal.tsx
@@ -84,6 +84,22 @@ export default function RegisterModal() {
         Icon={FcGoogle}
         onClick={() => {}}
       />
+      <Button
+        outline
+        label="Continue with Github"
+        Icon={AiFillGithub}
+        onClick={() => {}}
+      />
+      <div className="text-neutral-500 text-center mt-4 font-light">
+        <div className="flex flex-row items-center gap-2"></div>
+        <div>Already have an account?</div>
+        <div
+          className="text-neutral-800 cursor-pointer hover:underline"
+          onClick={() => registerModal.onClose()}
+        >
+          Log in
+        </div>
+      </div>
     </div>
   );
 
